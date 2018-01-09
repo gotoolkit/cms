ce2f20311e86df2e2191447d2ecd307cbfd92f95
llitfkitfk <llitfkitfk@gmail.com>
2018-01-09 19:40:30 +0800

    add mux

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gotoolkit/hook/handlers"
 )
 
 func main() {
 	log.Print("Starting the service...")
-
-	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, "Hello! Your request was processed.")
-	})
+	router := handlers.Router()
 	log.Print("The service is ready to listen and serve.")
 
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", router)
 }
