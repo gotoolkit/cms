a85f694763d9cbf65b7bf996316659b28d18b63a
llitfkitfk <llitfkitfk@gmail.com>
2018-01-09 19:32:40 +0800

    add logger

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Print("Starting the service...")
+
 	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, "Hello! Your request was processed.")
 	})
+	log.Print("The service is ready to listen and serve.")
+
 	http.ListenAndServe(":8000", nil)
 }
