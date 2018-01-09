8bf3e6d284531de663920c6cf4fa6df44b2e529b
llitfkitfk <llitfkitfk@gmail.com>
2018-01-09 21:55:03 +0800

    add config

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gotoolkit/hook/pkg/handlers"
 )
 
 func main() {
 	log.Print("Starting the service...")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
 	router := handlers.Router()
 	log.Print("The service is ready to listen and serve.")
 
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(":"+port, router)
 }
